c66165ed53ffec9027e36cc7f6cb4bf22e498011
Psyfo-C-YA <psyfohadebe@gmail.com>
2022-10-13 16:12:41 +0200

    Update on Authorising Access and Connecting to Server/Database

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DisasterAlleviationFoundation_prototype.Areas.Identity.Data;
+using DisasterAlleviationFoundation_prototype.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DisasterAlleviationFoundation_prototype.Areas.Identity.IdentityHostingStartup))]
+namespace DisasterAlleviationFoundation_prototype.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DisasterAlleviationFoundation_prototypeDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DisasterAlleviationFoundation_prototypeDBContextConnection")));
+
+                //services.AddDefaultIdentity<DisasterAlleviationFoundation_prototypeUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    //.AddEntityFrameworkStores<DisasterAlleviationFoundation_prototypeDBContext>();
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,8 @@ namespace DisasterAlleviationFoundation_prototype
 
             services.AddDbContext<DisasterAlleviationFoundation_prototypeContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DisasterAlleviationFoundation_prototypeContext")));
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +51,8 @@ namespace DisasterAlleviationFoundation_prototype
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -56,6 +60,7 @@ namespace DisasterAlleviationFoundation_prototype
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
